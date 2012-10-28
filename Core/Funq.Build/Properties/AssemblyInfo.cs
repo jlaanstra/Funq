a25c2cc7dfcfdf894624ca580f0f6ec0092779e0
Johan Laanstra <jlaanstra1221@hotmail.com>
2012-10-29 00:55:47 +0100

    Switched to portable library.
    Added Ms-PL license.

--- Core/Funq.Build/Properties/AssemblyInfo.cs
+++
@@ -1,4 +0,0 @@
-﻿using System.Reflection;
-
-[assembly: AssemblyTitle("Funq.Build")]
-[assembly: AssemblyDescription("MSBuild integration for auto-generating funqlets from referenced libraries.")]
---
+++ Funq/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Funq")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Clarius Consulting")]
+[assembly: AssemblyProduct("Funq Dependency Injection Framework")]
+[assembly: AssemblyCopyright("Copyright (C) Clarius Consulting")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
